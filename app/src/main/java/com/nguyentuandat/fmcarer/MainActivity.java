15e78958ef72a0cfe54a93689f067a95741c5cf0
Nguyentuandat <datntph39291@fpt.edu.vn>
2025-06-10 17:41:50 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyentuandat/fmcarer/MainActivity.java
@@ -0,0 +1,27 @@
+package com.nguyentuandat.fmcarer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.nguyentuandat.fmcarer.VIEW.Signin_Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Signin_Activity.class);
+                startActivity(intent);
+            }
+        }, 1000); // 3000 milliseconds = 3 seconds
+    }
+
+}
